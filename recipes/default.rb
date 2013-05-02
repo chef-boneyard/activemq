b95f4148b9209d6a04b02652fc05b90ce6177e22
Lyndon Washington <lwashington@netiq.com>
2013-05-02 15:24:57 -0400

    Fix the wrapper.conf
    No longer appear to need the linux link since no longer necessary to use the wrapper since the default arch-based one works out of the box.

--- recipes/default.rb
+++ recipes/default.rb
@@ -57,19 +57,8 @@ service "activemq" do
   action [:enable, :start]
 end
 
-# symlink so the default wrapper.conf can find the native wrapper library
-link "#{activemq_home}/bin/linux" do
-  to "#{activemq_home}/bin/linux-#{arch}"
-end
-
 # symlink the wrapper's pidfile location into /var/run
 link "/var/run/activemq.pid" do
   to "#{activemq_home}/bin/linux/ActiveMQ.pid"
   not_if "test -f /var/run/activemq.pid"
-end
-
-template "#{activemq_home}/bin/linux/wrapper.conf" do
-  source "wrapper.conf.erb"
-  mode 0644
-  notifies :restart, 'service[activemq]'
 end
