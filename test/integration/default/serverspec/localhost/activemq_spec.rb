a99965dfec7f8f410d66609e1cf17f59c5a929a2
Lyndon Washington <lwashington@netiq.com>
2013-09-05 20:57:18 -0400

    test-kitchen update
    Added specs for validating activemq service.

---
+++ test/integration/default/serverspec/localhost/activemq_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'ActiveMQ server' do
+
+  it 'should have a vagrant user' do
+    expect(user 'vagrant').to exist
+  end
+
+  it 'should be running the activemq server' do
+    expect(service 'activemq').to be_running
+    expect(service 'activemq').to be_enabled
+  end
+
+end
