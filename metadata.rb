2f8ec4104c9c69fb7c94e9cc7b31186d982ff11f
Tim Smith <tim@cozy.co>
2015-08-08 16:13:57 -0700

    Update to Chef Inc.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'activemq'
-maintainer       'Opscode, Inc.'
-maintainer_email 'cookbooks@opscode.com'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs activemq and sets it up as service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
