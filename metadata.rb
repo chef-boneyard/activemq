751ec8c1d22342ddb33a2d941ed42219a205e482
Tim Smith <tim@cozy.co>
2015-08-12 23:08:17 -0700

    Fix chef 12 metadata to not break chef 11

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs activemq and sets it up as service'
 version '1.3.5'
-source_url 'https://github.com/opscode-cookbooks/activemq'
-issues_url 'https://github.com/opscode-cookbooks/activemq/issues'
 
 %w(ubuntu debian redhat centos suse scientific oracle amazon).each do |os|
   supports os
 end
 
 depends 'java', '~> 1.13'
+
+source_url 'https://github.com/opscode-cookbooks/activemq' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/activemq/issues' if respond_to?(:source_url)
