8dfcbb1b8fe27b54149b04cca53ed061102e22a1
Tim Smith <tsmith84@gmail.com>
2015-08-31 17:07:45 -0700

    Fix serverspec runs in Test Kitchen

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,11 +1,4 @@
 require 'serverspec'
-require 'pathname'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-end
+# Required by serverspec
+set :backend, :exec
