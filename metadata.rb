5f855a18f1f9b8e5d3a07b175cfd02a5e574840b
Tim Smith <tsmith84@gmail.com>
2015-09-12 21:47:04 -0700

    Add long_description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,10 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs activemq and sets it up as service'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
+
+recipe 'activemq::default', 'Installs Apache ActiveMQ and sets up the service using the included init script.'
 
 %w(ubuntu debian redhat centos suse scientific oracle amazon).each do |os|
   supports os
@@ -11,6 +14,5 @@ end
 
 depends 'java', '~> 1.13'
 
-recipe 'activemq::default', 'Installs Apache ActiveMQ and sets up the service using the included init script.'
 source_url 'https://github.com/chef-cookbooks/activemq' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/activemq/issues' if respond_to?(:issues_url)
