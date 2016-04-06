2a92140f363e9b7419606c82d74ebdd8f6869465
Tim Smith <tsmith84@gmail.com>
2016-04-05 20:45:54 -0700

    Add install custom resource

--- test/cookbooks/test/attributes/default.rb
+++ test/cookbooks/test/attributes/default.rb
@@ -3,4 +3,3 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['set_etc_environment'] = true
 default['java']['oracle']['accept_oracle_download_terms'] = true
-default['activemq']['use_default_config'] = true
--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,2 +1,5 @@
 include_recipe 'java::default'
-include_recipe 'activemq::default'
+
+activemq_install 'test_instance' do
+  version '5.13.2'
+end
