6a7abec9f9d4f45a114799cfea73abb0332ab892
Tim Smith <tsmith@chef.io>
2017-03-21 15:44:19 -0700

    Update Apache 2.0 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'activemq'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs activemq and sets it up as service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.0'
