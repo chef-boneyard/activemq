702b7592135c2f162b444fd2866a8a26cc488310
Tim Smith <tsmith@chef.io>
2017-04-04 11:53:23 -0700

    Fix issues with action class on Chef 12.5/12.6
    Move the action class below the actions to work around a chef bug we’ve since fixed. Also use action_class.class_eval for 12.5 and 12.6
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/install.rb
+++ resources/install.rb
@@ -10,7 +10,58 @@ property :tarball_uri, String
 property :activemq_user, String, default: lazy { |r| "activemq_#{r.instance_name}" }
 property :activemq_group, String, default: lazy { |r| "activemq_#{r.instance_name}" }
 
-action_class do
+action :install do
+  validate_version
+
+  # some RHEL systems lack tar in their minimal install
+  package 'tar'
+
+  group new_resource.activemq_group do
+    action :create
+    append true
+  end
+
+  user new_resource.activemq_user do
+    gid new_resource.activemq_group
+    shell '/bin/false'
+    system true
+    action :create
+  end
+
+  directory 'activemq install dir' do
+    mode '0750'
+    path new_resource.install_path
+    recursive true
+    owner new_resource.activemq_user
+    group new_resource.activemq_group
+  end
+
+  remote_file "activemq #{new_resource.version} tarball" do
+    source tarball_uri
+    path "#{Chef::Config['file_cache_path']}/apache-activemq-#{new_resource.version}-bin.tar.gz"
+    verify { |file| validate_checksum(file) }
+  end
+
+  execute 'extract activemq tarball' do
+    command extraction_command
+    action :run
+    creates ::File.join(new_resource.install_path, 'LICENSE')
+  end
+
+  # make sure the instance's user owns the instance install dir
+  execute "chown install dir as activemq_#{new_resource.instance_name}" do
+    command "chown -R #{new_resource.activemq_user}:#{new_resource.activemq_group} #{new_resource.install_path}"
+    action :run
+    not_if { Etc.getpwuid(::File.stat("#{new_resource.install_path}/LICENSE").uid).name == new_resource.activemq_user }
+  end
+
+  # create a link that points to the latest version of the instance
+  link "/opt/activemq_#{new_resource.instance_name}" do
+    to new_resource.install_path
+  end
+end
+
+action_class.class_eval do
   # build the extraction command based on the passed properties
   def extraction_command
     cmd = "tar -xzf #{Chef::Config['file_cache_path']}/apache-activemq-#{new_resource.version}-bin.tar.gz -C #{new_resource.install_path} --strip-components=1"
@@ -78,56 +129,5 @@ action_class do
       uri << new_resource.tarball_uri
     end
     uri
-  end
-end
-
-action :install do
-  validate_version
-
-  # some RHEL systems lack tar in their minimal install
-  package 'tar'
-
-  group new_resource.activemq_group do
-    action :create
-    append true
-  end
-
-  user new_resource.activemq_user do
-    gid new_resource.activemq_group
-    shell '/bin/false'
-    system true
-    action :create
-  end
-
-  directory 'activemq install dir' do
-    mode '0750'
-    path new_resource.install_path
-    recursive true
-    owner new_resource.activemq_user
-    group new_resource.activemq_group
-  end
-
-  remote_file "activemq #{new_resource.version} tarball" do
-    source tarball_uri
-    path "#{Chef::Config['file_cache_path']}/apache-activemq-#{new_resource.version}-bin.tar.gz"
-    verify { |file| validate_checksum(file) }
-  end
-
-  execute 'extract activemq tarball' do
-    command extraction_command
-    action :run
-    creates ::File.join(new_resource.install_path, 'LICENSE')
-  end
-
-  # make sure the instance's user owns the instance install dir
-  execute "chown install dir as activemq_#{new_resource.instance_name}" do
-    command "chown -R #{new_resource.activemq_user}:#{new_resource.activemq_group} #{new_resource.install_path}"
-    action :run
-    not_if { Etc.getpwuid(::File.stat("#{new_resource.install_path}/LICENSE").uid).name == new_resource.activemq_user }
-  end
-
-  # create a link that points to the latest version of the instance
-  link "/opt/activemq_#{new_resource.instance_name}" do
-    to new_resource.install_path
   end
 end
