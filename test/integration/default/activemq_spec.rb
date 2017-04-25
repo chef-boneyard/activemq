9e322c104f917e8ab16e2bf5a073592c9804e8a1
Jennifer Davis <iennae@gmail.com>
2017-04-25 15:11:15 -0700

    Fix linting
    Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- test/integration/default/activemq_spec.rb
+++ test/integration/default/activemq_spec.rb
@@ -2,10 +2,10 @@ describe command('ps ax | grep activem[q]') do
   its('exit_status') { should eq 0 }
 end
 
-describe command("/opt/apache-activemq-5.14.4/bin/activemq producer") do
+describe command('/opt/apache-activemq-5.14.4/bin/activemq producer') do
   its('stdout') { should match(/Produced: 1000 messages/) }
 end
 
-describe command("/opt/apache-activemq-5.14.4/bin/activemq consumer") do
+describe command('/opt/apache-activemq-5.14.4/bin/activemq consumer') do
   its('stdout') { should match(/Consumed: 1000 messages/) }
 end
