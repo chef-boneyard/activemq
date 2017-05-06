01cd090d5fbc632d12320becab35ccd057ed1ade
Tim Smith <tsmith@chef.io>
2017-05-06 11:37:35 -0700

    Release 4.1.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs activemq and sets it up as service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '4.0.3'
+version '4.1.0'
 
 recipe 'activemq::default', 'Installs Apache ActiveMQ and sets up the service using the included init script.'
 
